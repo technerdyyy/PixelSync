e9f893741c0d92fa71f59792179d5dddf06ff3e4
Piyush Mishra <pm9958478949@gmail.com>
2025-04-03 21:19:39 +0530

    chat chating

--- backend/index.js
+++ backend/index.js
@@ -5,18 +5,17 @@ const connectDB = require("./config/connectDB");
 const cookiesParser = require('cookie-parser')
 const router = require('./routes/index')
 const http = require("http");
-const userRoutes = require("./routes/userRoutes");
-const authRoutes = require("./routes/authRoutes");
 const socketHandler = require("./socket"); // Import socket logic
 
 dotenv.config();
 const app = express();
-app.use(express.json());
-
 app.use(cors({
   origin: process.env.FRONTEND_URL|| "http://localhost:5173",  
     credentials: true 
 }))
+app.use(cookiesParser());
+app.use(express.json());
+
 
 // Connect to Database
 connectDB();
@@ -37,8 +36,7 @@ const io = require("socket.io")(server, {
 socketHandler(io);
 
 // Routes
-app.use("/api/users", userRoutes);
-app.use("/api/auth", authRoutes);
+app.use('/api',router)
 // Start server
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
